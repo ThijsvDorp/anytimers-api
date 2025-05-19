a8f7f96be8687f7e7fc72226452ec4f27c0f0f54
thijsvdev <thijs.vandorp@nedap.com>
2025-05-19 16:25:18 +0200

    removing unused imports

--- src/main/java/com/anytimers/api/domain/auth/controller/AuthController.java
+++ src/main/java/com/anytimers/api/domain/auth/controller/AuthController.java
@@ -9,14 +9,7 @@ import com.anytimers.api.domain.auth.service.AuthService;
 
 import jakarta.validation.Valid;
 
-import java.util.List;
-
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
